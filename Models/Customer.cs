e52e2b023b4c6cd350e2e6c0ae24c65ac542e429
Aparnanibha <aparnanibha@gmail.com>
2022-04-12 23:58:33 +0530

    Added validation in Views instead of model

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -13,7 +13,7 @@ namespace BankingManagementSystem.Models
 
         public string CustomerId { get; set; } = null!;
 
-        [Required]
+       // [Required]
         [Range(1, 16, ErrorMessage = "First name is too short or too long! Please Enter Valid Details")]
         [Display(Name = "Enter Your First Name")]
         public string? FirstName { get; set; }
@@ -21,7 +21,7 @@ namespace BankingManagementSystem.Models
         [Display(Name = "Enter Your Middle Name")]
         public string? MiddleName { get; set; }
 
-        [Required]
+       // [Required]
         [Display(Name = "Enter Your Last Name")]
         [Range(1, 16, ErrorMessage = "Last name is too short or too long! Please enter valid details")]
         public string? LastName { get; set; }
@@ -39,40 +39,40 @@ namespace BankingManagementSystem.Models
 
         [DataType(DataType.Date)]
         [Display(Name = "Enter your Date of Birth")]
-        [Required]
+      //  [Required]
         public DateTime? DateOfBirth { get; set; }
 
         [DataType(DataType.MultilineText)]
         [Display(Name = "Enter your Residential Address")]
-        [Required]
+       // [Required]
         public string? ResidentialAddress { get; set; }
 
         [DataType(DataType.MultilineText)]
         [Display(Name = "Enter your Permanent Address")]
-        [Required]
+        //[Required]
         public string? PermanentAddress { get; set; }
 
-        [Required]
+       // [Required]
         [Display(Name = "Occupation Type")]
         public string? OccupationType { get; set; }
 
-        [Required]
+       // [Required]
         [Display(Name = "Source of Income")]
         public string? SourceOfIncome { get; set; }
 
-        [Required]
+       // [Required]
         [Display(Name = "Gross Annual Income")]
         public decimal? GrossAnnualIncome { get; set; }
 
         [Display(Name = "Do you want Debit Card?(Yes/No)")]
-        [Required]
+       // [Required]
         public string? IsDebitCardRequired { get; set; }
 
         [Display(Name = "Do you want to enable NetBanking?(Yes/No)")]
-        [Required]
+      //  [Required]
         public string? EnableNetBanking { get; set; }
 
-        [Required]
+       // [Required]
         [Display(Name = "Enter your Aadhar Card Number")]
         public string? AadharNumber { get; set; }
 
